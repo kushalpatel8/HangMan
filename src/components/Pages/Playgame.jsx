dd12c945c723718f495fda5ca38491559e836e98
Kushal Patel <pkushal649@gmail.com>
2025-02-01 13:13:18 +0530

    LetterButtons

---
+++ src/components/Pages/Playgame.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+/* eslint-disable no-undef */
+function PlayGame() {
+
+    const { wordList, word } = useWordStore();
+    const [guessedLetters, setGuessedLetters] = useState([]);
+    const [step, setStep] = useState(0);
+``
+    return (
+        <>
+        </>
+    )
+}
+
+export default PlayGame;
