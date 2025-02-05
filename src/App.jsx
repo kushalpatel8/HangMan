8829d613b5d20d539f663fe1af0bdf1048c99155
Kushal Patel <pkushal649@gmail.com>
2025-02-06 00:02:41 +0530

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+import WordContext from "./context/WordContext";
+import Routes from "./routes";
+import { Route } from "react-router-dom";
+import StartGame from "./src/components/Pages/Startgame";
+import PlayGame from "./src/components/Pages/Playgame";
+import Home from "./src/components/Pages/Home";
+
 function App () {
-    return (
-        <>
-        </>
+
+    const [wordList, setWordList] = useState([]);
+    const [word, setWord] = useState("");
+
+    return(
+        <WordContext.Provider value={{wordList, setWordList, word, setWord}}>  
+        <Routes>
+            <Route path = '/start' element = {<StartGame/>}/>
+            <Route path = '/play' element = {<PlayGame/>} />
+            <Route path = '/' element = {<Home/>} />
+        </Routes>
+        </WordContext.Provider>
     );
 }
 
